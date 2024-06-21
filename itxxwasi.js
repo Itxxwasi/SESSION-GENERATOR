59bf668c932e40711fc580079f08d2ab9358d7c8
Ntee-j01 <140890199+Ntee-j01@users.noreply.github.com>
2024-06-22 01:15:31 +0200

    Update itxxwasi.js

--- itxxwasi.js
+++ itxxwasi.js
@@ -12,7 +12,7 @@ app.use('/pair',async (req, res, next) => {
 res.sendFile(__path + '/pair.html')
 })
 app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/wasipage.html')
+res.sendFile(__path + '/nteejpage.html')
 })
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
