9541e6f036c0925b8e5c6bba88745859df5045e3
wasi tech <wasitech10@gmail.com>
2024-06-29 13:58:41 +0100

    Update itxxwasi.js

--- itxxwasi.js
+++ itxxwasi.js
@@ -3,7 +3,7 @@ const app = express();
 __path = process.cwd()
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let server = require('./wasiqr'),
+let server = require('./qrr'),
     code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
 app.use('/wasiqr', server);
