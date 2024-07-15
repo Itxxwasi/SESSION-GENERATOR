4b3e5949e6693599886b0a4dee9d464addaa6189
popkid-ke <kidreen254@gmail.com>
2024-07-15 15:51:56 +0300

    Update qrr.js

--- qrr.js
+++ qrr.js
@@ -17,14 +17,14 @@ const { Boom } = require("@hapi/boom");
 const PORT = process.env.PORT ||  5000
 const MESSAGE = process.env.MESSAGE ||  `
 ┌───⭓『
-❒ *WASI-MD*
+❒ *POPKID-MD*
 ❒ _NOW DEPLOY IT_
 └────────────⭓
 ┌───⭓
 ❒  • Chat with owner •
-❒ *GitHub:* __https://github.com/WASI-MD_
-❒ *Author:* _wa.me/923192173398_
-❒ *YT:* _https://youtube.com/@wasitech10_
+❒ *GitHub:* __https://github.com/popkid-ke_
+❒ *Author:* _wa.me/254111385747_
+❒ *YT:* _https://youtube.com/@popkid10_
 └────────────⭓
 `
 
