9880ee96ea3453ad9707b90f8de0bb60e69fce6d
sarKarji1 <149999688+sarKarji1@users.noreply.github.com>
2024-11-03 09:03:26 +0500

    Update id.js

--- id.js
+++ id.js
@@ -1,10 +1,11 @@
 function makeid(num = 4) {
   let result = "";
-  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   var characters9 = characters.length;
   for (var i = 0; i < num; i++) {
     result += characters.charAt(Math.floor(Math.random() * characters9));
   }
   return result;
 }
-module.exports = {makeid};
+module.exports = { makeid };
